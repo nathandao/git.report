65c6280e59e81b665897ade68b2590083c13e0f2
nathandao <nathan@guynathan.com>
2016-01-19 18:12:37 +0200

    update websocket config and add separate rainbows stream

--- lib/ginatra/web/websocket_server.rb
+++ lib/ginatra/web/websocket_server.rb
@@ -3,28 +3,22 @@ require 'eventmachine'
 module Ginatra
   class WebsocketServer
     attr_accessor :channel, :scheduler
-
     class << self
       def start
         websocket_port = Ginatra::Env.websocket_port || 9290
-
         @channel = EM::Channel.new
-
         EM::WebSocket.start(:host => '0.0.0.0', :port => websocket_port) do |ws|
           ws.onopen do |handshake|
             sid = @channel.subscribe { |msg| ws.send msg }
-
             ws.onclose do
               @channel.unsubscribe(sid)
               ws = nil
             end
           end
-
           # hit Control + C to stop
           Signal.trap("INT")  { EventMachine.stop }
           Signal.trap("TERM") { EventMachine.stop }
         end
-
         Ginatra::Stat.start_repo_streams(@channel, Ginatra::Config.update_interval)
       end
     end
