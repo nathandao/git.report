c2ed944bff286223e56dd8acfa766d7ab6fe71df
nathandao <nathan@guynathan.com>
2016-01-20 06:28:56 +0200

    refactor

--- assets/js/src/routes.js
+++ assets/js/src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Router, Route, IndexRoute } from 'react-router';
 
-import App from 'App';
+import App from 'app';
 import Home from 'pages/Home';
 import Selector from 'pages/Selector';
 
